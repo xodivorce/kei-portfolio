cc84f5648bde5af1bfe67cd687460738e593354f
Prasid Mandal <prasidmandal79@gmail.com>
2024-02-11 16:00:46 +0530

    added dark mode beta & redesinged the webpage

--- script.js
+++ script.js
@@ -24,3 +24,16 @@ document.addEventListener("DOMContentLoaded", function() {
       document.querySelector(".preloader").style.display = "none";
   }, 1500);
 });
+var icon = document.getElementById("icon");
+icon.onclick = function(){
+    document.body.classList.toggle("dark-theme");
+    if(document.body.classList.contains("dark-theme")){
+      icon.src = "assets/images/sun.png";
+    }else{
+      icon.src = "assets/images/moon.png";
+    }
+}
+
+function closePopup() {
+  document.getElementById('overlay').style.display = 'none';
+}
